3d456ef140fc99d66cfd09f7b91e2665438314c5
jh3389 <jh3389@columbia.edu>
2017-06-13 16:39:40 -0400

    refactor

--- web/src/main/java/edu/columbia/cuitei/deptdir/controller/DeptDirController.java
+++ web/src/main/java/edu/columbia/cuitei/deptdir/controller/DeptDirController.java
@@ -9,6 +9,8 @@ import edu.columbia.cuitei.deptdir.service.Level4Service;
 import edu.columbia.cuitei.deptdir.service.QueryService;
 import java.util.List;
 import javax.annotation.Resource;
+
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
@@ -156,6 +158,7 @@ public class DeptDirController {
             model.addAttribute("feedback", "");
         } else {
             final List<Directory> list = queryService.search("%" + term + "%");
+            formatName(list);
             model.addAttribute("directories", list);
             model.addAttribute("searchTerm", term);
             if( list.isEmpty() ) {
@@ -182,4 +185,79 @@ public class DeptDirController {
         return "redirect:/search";
     }
 
+    private void formatName(List<Directory> list) {
+        for (Directory d : list) {
+            if( "LEVEL1".equals(d.getLevel())) {
+                formatLevel1Name(d);
+            }
+        }
+    }
+
+    private void formatLevel1Name(Directory d) {
+        String address = d.getAddress();
+        if( !StringUtils.isBlank(address) ) {
+            //address = "</br>Address:&nbsp;&nbsp;" + address;
+            address = "\n  " + address;
+        }
+        String mailCode = d.getMailCode();
+        if( !StringUtils.isBlank(mailCode)) {
+            //mailCode = "&nbsp;&nbsp;Mail Code:&nbsp;&nbsp;" + mailCode;
+            mailCode = " Mail Code:  " + mailCode;
+        }
+
+        String phone = d.getPhoneNumber();
+        if( !StringUtils.isBlank(phone)) {
+            //phone = "&nbsp;&nbsp;Phone: &nbsp;&nbsp;"+ phone + "&nbsp;&nbsp;";
+            phone = "  Phone:  "+ phone + "  ";
+        }
+
+        String tieLine  = d.getTieLine();
+        if( !StringUtils.isBlank(tieLine)) {
+            //tieLine = "Tie Line: &nbsp;&nbsp;"+ tieLine;
+            tieLine = "Tie Line:   "+ tieLine;
+        }
+
+        //String name = d.getName() + address + mailCode + phone + "&nbsp;&nbsp;" + tieLine;
+        String name = d.getName() + "<br>" + address + mailCode + phone + "  " + tieLine;
+        d.setName(name);
+
+    }
+
+
+    /*
+    function getLevel1(data) {
+    var bg = '<tr class="active"><td>';
+    var row = "<strong>" + data.name + "</strong>";
+    var address = "";
+    if (data.address != "") {
+        address = "</br>Address:&nbsp;&nbsp;" + data.address;
+    }
+    var mailCode = "";
+    if (data.mailCode != "") {
+        if(address!="") {
+            mailCode = "&nbsp;&nbsp;Mail Code:&nbsp;&nbsp;" + data.mailCode;
+        }else {
+            mailCode = "<br/>MAILCODE:&nbsp;&nbsp;" + data.mailCode;
+        }
+    }else {
+        if(address!="") {
+            mailCode = "&nbsp;&nbsp;";
+        }else {
+            mailCode = "<br/>";
+        }
+    }
+    var phone="";
+    if( data.phoneNumber != "") {
+        phone = "&nbsp;&nbsp;Phone: &nbsp;&nbsp;"+data.phoneNumber + '&nbsp;&nbsp;';
+    }
+    var tieLine="";
+    if( data.tieLine != "") {
+        tieLine = "Tie Line: &nbsp;&nbsp;"+data.tieLine;
+    }
+
+    row = bg + row + address + mailCode + phone + '&nbsp;&nbsp;' + tieLine + '</td></tr>';
+    return row;
+}
+
+     */
 }
